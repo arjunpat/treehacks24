688b3e59217ed9b7e450f50d27c3b80772840740
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-02-17 14:14:22 -0800

    started response page

---
+++ baib/src/routes/response/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Source from '$lib/components/Source.svelte';
+
+	let question = "What is Tony's birthday?";
+	let answer = "Tony's birthday is on October 10th";
+</script>
+
+<div class="container h-full mx-auto p-4 flex flex-col gap-4">
+	<div>
+		<div class="font-bold text-xs">HEY BAIB,</div>
+		<div class="text-2xl">{question}</div>
+	</div>
+
+	<div>
+		<h4 class="h4 mb-2">Sources</h4>
+		<Source message="my birthday is on oct 10th LMAO" />
+	</div>
+
+	<div>
+		<h4 class="h4 mb-2">Answer</h4>
+		<div>{answer}</div>
+	</div>
+</div>
