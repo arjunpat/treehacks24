0f8a714bd8bbe5385fca40bd312683cc5956956e
Tony Xin <32372239+TtonyxX@users.noreply.github.com>
2024-02-18 00:17:56 -0800

    Update PromptInput.svelte

--- baib/src/lib/components/PromptInput.svelte
+++ baib/src/lib/components/PromptInput.svelte
@@ -16,8 +16,9 @@
 {#if ready}
 	<div class="mt-2 flex gap-1 w-full" transition:fade>
 		<input class="input rounded-lg" bind:value={input} placeholder="ask anything :)" />
+    <a href="/response">
 		<button type="button" class="shrink-0 btn-icon variant-filled-surface"
 			><ArrowRightIcon /></button
-		>
+		></a>
 	</div>
 {/if}
