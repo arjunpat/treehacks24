35a38cb50eed6d5fcdab979a49edd42e81e21526
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-02-18 02:34:34 -0800

    everything works now!!

--- baib/src/lib/components/AnswerSection.svelte
+++ baib/src/lib/components/AnswerSection.svelte
@@ -1,23 +1,24 @@
 <script lang="ts">
 	import type { AnswerContent } from '$lib/types';
 
-	export let answer: AnswerContent[] = [];
+	export let answer: any[] = [];
+	export let citations: any = {};
 </script>
 
 <div class="space-y-4">
 	{#each answer as a}
 		{#if a.type === 'text'}
-			<div class="text-sm">{a.content.text}</div>
+			<div class="text-sm">{a.text}</div>
 		{:else if a.type === 'message'}
 			<div class="card p-2 text-sm space-y-2 flex flex-col">
-				{#each a.content.messages as message}
-					{#if message.speaker === 'other'}
-						<div class="px-2 py-2 rounded-xl bg-[#3B3B3D] max-w-72">
-							{message.text}
+				{#each a.messages as id}
+					{#if citations[id]?.speaker === 'other'}
+						<div class="px-2 py-2 rounded-xl bg-[#3B3B3D] max-w-72 self-start">
+							{citations[id]?.text}
 						</div>
-					{:else if message.speaker === 'self'}
+					{:else if citations[id]?.speaker === 'self'}
 						<div class="px-2 py-2 rounded-xl bg-[#1A89FF] max-w-72 self-end">
-							{message.text}
+							{citations[id]?.text}
 						</div>
 					{/if}
 				{/each}
--- baib/src/routes/response/+page.svelte
+++ baib/src/routes/response/+page.svelte
@@ -9,44 +9,22 @@
 	import { onDestroy, onMount } from 'svelte';
 	import { fade } from 'svelte/transition';
 
-	// const WEBSOCKET_URL = 'wss://l6xbzhkc-8000.usw3.devtunnels.ms';
-	const WEBSOCKET_URL = 'ws://localhost:8000';
+	const WEBSOCKET_URL = 'wss://l6xbzhkc-8000.usw3.devtunnels.ms';
+	// const WEBSOCKET_URL = 'ws://localhost:8000';
 
-	let answer: AnswerContent[] = [
+	let citations = {
+		1: { speaker: 'other', text: 'Yo olivia and i are having our 2 year this weekend' },
+		2: { speaker: 'other', text: 'hahah' },
+		3: { speaker: 'self', text: 'niceeeee' }
+	};
+	let answer = [
 		{
 			type: 'text',
-			content: {
-				text: `Based on the text messages, it appears Tony Xin has expressed romantic interest in at least one individual named Olivia. This is evidenced by a message where Tony mentions hanging out with Olivia because it is their "2 year", indicating a significant anniversary that suggests a romantic relationship.`
-			}
+			text: `Based on the text messages, it appears Tony Xin has expressed romantic interest in at least one individual named Olivia. This is evidenced by a message where Tony mentions hanging out with Olivia because it is their "2 year", indicating a significant anniversary that suggests a romantic relationship.`
 		},
 		{
 			type: 'message',
-			content: {
-				citationId: '1',
-				messages: [
-					{ speaker: 'other', text: 'Yo Olivia and I are having our 2 year this weekend' },
-					{ speaker: 'self', text: 'niceeee' }
-				]
-			}
-		},
-		{
-			type: 'text',
-			content: {
-				text: `Additionally, there's another instance where Tony mentions that Olivia is staying over, which could imply a close personal relationship.`
-			}
-		},
-		{
-			type: 'message',
-			content: {
-				citationId: '2',
-				messages: [
-					{ speaker: 'other', text: 'Olivia is staying over imma need the room to myself >:)' },
-					{
-						speaker: 'self',
-						text: 'hahhaha sounds good ksdfkasjdfkajds fkjsdf kajdf jadkf jasdkf jaskdfj aksdfjaksdjf aksdjfa ksdjf kadjf aksdj fkj'
-					}
-				]
-			}
+			messages: [1, 2, 3]
 		}
 	];
 
@@ -57,11 +35,44 @@
 	// 	}, 3000);
 	// });
 
+	function parseAnswer(data: { answer: string; citations: any }) {
+		answer = [];
+		citations = data.citations;
+
+		let left = data.answer;
+		while (left.length > 0) {
+			const start = left.indexOf('{{');
+			const end = left.indexOf('}}', start);
+			if (start === -1 || end === -1) break;
+
+			const text = left.substring(0, start);
+			const citationNum = parseInt(left.substring(start + 2, end));
+
+			answer.push({ type: 'text', text: text });
+			answer.push({ type: 'message', messages: [citationNum - 1, citationNum, citationNum + 1] });
+
+			left = left.substring(end + 2);
+		}
+		if (left.length > 0) {
+			answer.push({ type: 'text', text: left });
+		}
+	}
+
 	// Run generate api call
 	let messageProgress: Progress = { done: false, text: '' };
 
 	let socket: WebSocket;
 	function init() {
+		// parseAnswer({
+		// 	answer: `Tony Xin's birthday is on October 22nd. This information comes from a text message sent on October 22, 2021, at 08:22 AM, where the sender wishes Tony Xin a "big boy happy birthday" and mentions seeing him later that day {{1457}}. This message clearly indicates that October 22nd is the day Tony Xin celebrates his birthday.`,
+		// 	citations: {
+		// 		1456: { speaker: 'other', text: 'Have not done extra credit' },
+		// 		1457: { speaker: 'self', text: 'TONY XIN big boy happy birthday see you later today ' },
+		// 		1458: { speaker: 'other', text: 'THANK U ARJUN PAT' }
+		// 	}
+		// });
+		// console.log(answer);
+		// return;
 		input = '';
 
 		if ($query.length === 0) {
@@ -74,7 +85,7 @@
 
 		socket = new WebSocket(`${WEBSOCKET_URL}/generate`);
 		socket.onopen = (event) => {
-			socket.send(JSON.stringify({ question: e.detail.text }));
+			socket.send(JSON.stringify({ question: $query }));
 		};
 		socket.onmessage = (event) => {
 			const data = JSON.parse(event.data);
@@ -91,8 +102,10 @@
 					messageProgress.text = `Looking for texts related to ${keywords}...`;
 				}
 			} else if (data.status === 'success') {
+				console.log('answer: ', data);
 				messageProgress.done = true;
 				showAnswer = true;
+				parseAnswer(data);
 			}
 		};
 	}
@@ -136,7 +149,7 @@
 		<div transition:fade>
 			<h4 class="h4 mb-2">Answer</h4>
 			<ImageSection text={$query}></ImageSection>
-			<AnswerSection {answer} />
+			<AnswerSection {answer} {citations} />
 		</div>
 	{/if}
 
