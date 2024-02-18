e48069e57594a3dfd3b0babf23a960daf4362070
Tony Xin <32372239+TtonyxX@users.noreply.github.com>
2024-02-17 17:05:51 -0800

    finished front page

--- baib/src/routes/+page.svelte
+++ baib/src/routes/+page.svelte
@@ -2,12 +2,19 @@
 
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import logo from "../assets/logo.png";
+	import logo from '../assets/logo.png';
+	import imLogo from '../assets/imessage.png';
+	import gmailLogo from '../assets/gmail.png';
+	import gPhotos from '../assets/gphotos.png';
+	import ArrowRightIcon from '~icons/mdi/arrow-right';
+	import { fade, scale } from 'svelte/transition';
 
-	let input = ""
+	let input = '';
 
 	let questionFinal = 'What do you want help with today?';
 	let question = '';
+
+	let showCursor = false;
 
 	function revealText() {
 		const characters = questionFinal.split('');
@@ -19,34 +26,84 @@
 		});
 	}
 
+	let ready = false;
+	let interval;
+
+	function showHideCursor() {
+		setInterval(() => {
+			showCursor = !showCursor;
+		}, 500);
+	}
+
 	onMount(() => {
 		revealText();
+		ready = true;
+		showHideCursor();
 	});
 </script>
 
 <div class="container h-full mx-auto flex justify-center items-center p-5 w-full">
 	<div class="w-full">
 		<div class="flex gap-1 items-center flex-row">
-			
-		<h1 class="text-4xl">BAIB</h1></div>
+			<h1 class="text-4xl">BAIB</h1>
+		</div>
 
 		<div class="container flex justify-center flex-col w-full items-center gap-2 mt-20">
 			<h1 class="h1">Hey Arjun</h1>
 			<div class="flex">
 				<p>{question}</p>
-					<div class="bg-red-200 h-5 w-0.5 fading-div"></div>
+
+				<div class="h-5 w-0.5">
+					{#if showCursor}
+						<div transition:fade class="bg-red-200 h-5 w-0.5 fading-div"></div>
+					{/if}
+				</div>
 			</div>
 
+			{#if ready}
+				<img
+					alt="logo"
+					transition:scale
+					class="rounded-[50%] h-32 mt-6 border-2 border-white"
+					src={logo}
+				/>
+			{/if}
 
-			<img class="rounded-[50%] h-32 mt-6 border-2 border-white" src={logo}/>
+			{#if ready}
+				<div class="mt-10 flex gap-1 w-full" transition:fade>
+					<input class="input rounded-lg" bind:value={input} placeholder="enter your name" />
+					<button type="button" class="btn-icon variant-filled-surface"><ArrowRightIcon /></button>
+				</div>
+			{/if}
 
-			<div class="mt-10">
-				<input class="input rounded-lg" bind:value={input} placeholder="enter your name" />
-				<button type="button" class="btn-icon variant-filled">send</button>
+			{#if ready}
+				<div class="w-full mt-5" transition:fade>
+					<h1>Searches through</h1>
+					<div class="card p-4 mt-1 flex items-center justify-between">
+						<div class="flex gap-2 items-center">
+							<img class="w-5 h-5" alt="imessage" src={imLogo} />
+							<p>imessage</p>
+						</div>
+						<div><p class="text-gray-500">connected</p></div>
+					</div>
 
+					<div class="card p-4 mt-1 flex items-center justify-between">
+						<div class="flex gap-2 items-center">
+							<img class="w-5 h-4" alt="gmail" src={gmailLogo} />
+							<p>gmail</p>
+						</div>
+						<div><p class="text-gray-500">connected</p></div>
+					</div>
 
-
-			</div>
+					<div class="card p-4 mt-1 flex items-center justify-between">
+						<div class="flex gap-2 items-center">
+							<img class="w-5 h-5" alt="googlephotos" src={gPhotos} />
+							<p>google photos</p>
+						</div>
+						<div><p class="text-gray-500">connected</p></div>
+					</div>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
