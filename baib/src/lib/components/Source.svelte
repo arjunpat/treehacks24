30dbf8177ed37055d3af19572f85e362eefab711
Jonathan Liu <liu.z.jonathan@gmail.com>
2024-02-17 19:32:30 -0800

    animation ++

--- baib/src/lib/components/Source.svelte
+++ baib/src/lib/components/Source.svelte
@@ -11,7 +11,9 @@
 	// Props
 	export let message = '';
 	export let type: SourceType;
+	export let delay = 0;
 
+	let visible = false;
 	let loading = true;
 
 	let showCheckmark = false;
@@ -22,33 +24,36 @@
 	}
 
 	onMount(() => {
-		setTimeout(() => (loading = false), 1000);
+		setTimeout(() => (visible = true), delay);
+		setTimeout(() => (loading = false), 1000 + delay);
 	});
 </script>
 
-<div class="card flex items-center p-4 gap-2">
-	<div class="shrink-0 w-5 h-6 flex items-center">
-		{#if loading}
-			<span transition:fade class="absolute">
-				<ProgressRadial width="w-5" />
-			</span>
-		{:else if showCheckmark}
-			<span transition:fade class="absolute">
-				<AnimatedCheck delay={300} />
-			</span>
-		{:else}
-			<span transition:fade class="absolute">
-				{#if type === 'message'}
-					<MessageIcon />
-				{:else if type === 'email'}
-					<EmailIcon />
-				{:else if type === 'photo'}
-					<PhotoIcon />
-				{/if}
-			</span>
+{#if visible}
+	<div transition:slide class="card flex items-center p-4 gap-2">
+		<div class="shrink-0 w-5 h-6 flex items-center">
+			{#if loading}
+				<span transition:fade class="absolute">
+					<ProgressRadial width="w-5" />
+				</span>
+			{:else if showCheckmark}
+				<span transition:fade class="absolute">
+					<AnimatedCheck delay={300} />
+				</span>
+			{:else}
+				<span transition:fade class="absolute">
+					{#if type === 'message'}
+						<MessageIcon />
+					{:else if type === 'email'}
+						<EmailIcon />
+					{:else if type === 'photo'}
+						<PhotoIcon />
+					{/if}
+				</span>
+			{/if}
+		</div>
+		{#if !loading && !showCheckmark}
+			<div transition:slide={{ delay: 100 }} class="text-sm">"{message}"</div>
 		{/if}
 	</div>
-	{#if !loading && !showCheckmark}
-		<div transition:slide={{ delay: 100 }} class="text-sm">"{message}"</div>
-	{/if}
-</div>
+{/if}
